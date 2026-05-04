800889531f54ad475afda5f85414c0d44b6340cf
Luisa Arévalo <larevalo173@gmail.com>
2026-05-03 23:48:07 -0600

    chore: Adding the rest of the CRUD

--- src/main/java/com/uca/pokedexcapas012026/controller/PokedexController.java
+++ src/main/java/com/uca/pokedexcapas012026/controller/PokedexController.java
@@ -5,6 +5,8 @@ import jakarta.websocket.server.PathParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/pokedex/pokemon")
 public class PokedexController {
@@ -16,15 +18,32 @@ public class PokedexController {
 
     }
 
+    @GetMapping
+    public List<Pokemon> findAll(){
+        return pokedexService.findAllPokemon();
+    }
+
     @GetMapping("/{id}")
     public Pokemon getPokemonById(@PathParam("id") int id) {
         return pokedexService.findPokemonById(id);
     }
 
-    @PostMapping("")
+    @PostMapping
     public ResponseEntity<String> createPokemon(@RequestBody Pokemon pokemon) {
         pokedexService.createPokemon(pokemon);
         return ResponseEntity.ok("Pokemon has been created");
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updatePokemon(@RequestBody Pokemon pokemon) {
+        pokedexService.updatePokemon(pokemon);
+        return ResponseEntity.ok("Pokemon has been updated");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePokemon(@PathVariable int id) {
+        pokedexService.deletePokemonById(id);
+        return ResponseEntity.ok("Pokemon has been deleted");
     }
 
 }
