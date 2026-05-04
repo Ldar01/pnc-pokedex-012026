a33d0d41a6d2ac538913a419a672cddcbc45432d
Luisa Arévalo <larevalo173@gmail.com>
2026-05-03 23:49:12 -0600

    refactor: Change Get Pokemon By ID to Path Variable

--- src/main/java/com/uca/pokedexcapas012026/controller/PokedexController.java
+++ src/main/java/com/uca/pokedexcapas012026/controller/PokedexController.java
@@ -1,7 +1,6 @@
 package com.uca.pokedexcapas012026.controller;
 import com.uca.pokedexcapas012026.entities.Pokemon;
 import com.uca.pokedexcapas012026.service.PokedexService;
-import jakarta.websocket.server.PathParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +23,7 @@ public class PokedexController {
     }
 
     @GetMapping("/{id}")
-    public Pokemon getPokemonById(@PathParam("id") int id) {
+    public Pokemon getPokemonById(@PathVariable int id) {
         return pokedexService.findPokemonById(id);
     }
 
