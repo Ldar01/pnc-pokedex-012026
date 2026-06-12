00a995959f48a5f9e173b7c5ef1b5c611f2ebb35
Luisa Arévalo <larevalo173@gmail.com>
2026-06-12 10:43:30 -0600

    chore: Fixed role issue

--- src/main/java/com/uca/pokedexcapas012026/configuration/SecurityConfiguration.java
+++ src/main/java/com/uca/pokedexcapas012026/configuration/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,6 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     @Bean
